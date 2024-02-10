adf12db6ce4101962754c51b82223e439b51393c
ParsaChehrazi <159032386+ParsaChehrazi@users.noreply.github.com>
2024-02-10 21:39:58 +0330

    Add files via upload

---
+++ Exrc2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char let1 = 80 ;
+    char let2 = 97 ;
+    char let3 = 114 ;
+    char let4 = 115 ;
+    char let5 = 97 ;
+    char space = 32 ;
+    char let6 = 67 ;
+    char let7 = 104 ;
+    char let8 = 101 ;
+    char let9 = 104 ;
+    char let10 = 114 ;
+    char let11 = 97 ;
+    char let12 = 122 ;
+    char let13 = 105 ;
+
+     string result = "";
+    result += let1;
+    result += let2;
+    result += let3;
+    result += let4;
+    result += let5;
+    result += space;
+    result += let6;
+    result += let7;
+    result += let8;
+    result += let9;
+    result += let10;
+    result += let11;
+    result += let12;
+    result += let13;
+    
+    cout << result << endl;
+
+    return 0;
+}
