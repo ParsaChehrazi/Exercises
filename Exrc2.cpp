0b2d18d246a781dfde4f8a469fc92f8a5f9dc75a
ParsaChehrazi <159032386+ParsaChehrazi@users.noreply.github.com>
2024-02-10 21:46:55 +0330

    Update Exrc2.cpp

--- Exrc2.cpp
+++ Exrc2.cpp
@@ -3,36 +3,24 @@
 using namespace std;
 
 int main() {
-    char let1 = 80 ;
-    char let2 = 97 ;
-    char let3 = 114 ;
-    char let4 = 115 ;
-    char let5 = 97 ;
-    char space = 32 ;
-    char let6 = 67 ;
-    char let7 = 104 ;
-    char let8 = 101 ;
-    char let9 = 104 ;
-    char let10 = 114 ;
-    char let11 = 97 ;
-    char let12 = 122 ;
-    char let13 = 105 ;
+    
 
      string result = "";
-    result += let1;
-    result += let2;
-    result += let3;
-    result += let4;
-    result += let5;
-    result += space;
-    result += let6;
-    result += let7;
-    result += let8;
-    result += let9;
-    result += let10;
-    result += let11;
-    result += let12;
-    result += let13;
+    result += 80 ;
+    result += 97 ;
+    result += 114 ;
+    result += 115 ;
+    result += 97 ;
+    result += 32 ;
+    result += 67 ;
+    result += 104 ;
+    result += 101 ;
+    result += 104 ;
+    result += 114 ;
+    result += 97 ;
+    result += 122 ;
+    result += 105 ;  
+   
     
     cout << result << endl;
 
